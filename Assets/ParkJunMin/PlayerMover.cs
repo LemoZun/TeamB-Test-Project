639124aa1e4d92252a6774113ce9e979cb309961
Hanonkim <72588898+Hanonkim@users.noreply.github.com>
2024-08-21 17:37:39 +0900

    modify junmin`s code
    modify junmin`s code

--- Assets/ParkJunMin/PlayerMover.cs
+++ Assets/ParkJunMin/PlayerMover.cs
@@ -2,46 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMover : MonoBehaviour
+namespace Hanonkim
 {
-    // Start is called before the first frame update
-    public int movePower;
-    public bool check;
-    public string text;
-
-    public Collider collider;
-    public Rigidbody rigidbody;
-
-
-    private void Start() // 시작할때 한번 호출되는 메서드
+    public partial class PlayerMover : MonoBehaviour
     {
-        rigidbody.AddForce(Vector3.up * movePower, ForceMode.Impulse);
+        private int nextCnt = 0;
+        private int prevCnt = 0;
+        public int CriticalCnt { get; set; }
     }
+}
 
-    private void Update()
+namespace ParkJunMin
+{
+    public class PlayerMover : MonoBehaviour
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        // Start is called before the first frame update
+        public int movePower;
+        public bool check;
+        public string text;
+
+        public Collider collider;
+        public Rigidbody rigidbody;
+
+
+        private void Start() // 시작할때 한번 호출되는 메서드
         {
-            rigidbody.AddForce(Vector3.left * movePower, ForceMode.Force);
+            rigidbody.AddForce(Vector3.up * movePower, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+
+        private void Update()
         {
-            rigidbody.AddForce(Vector3.right * movePower, ForceMode.Force);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rigidbody.AddForce(Vector3.left * movePower, ForceMode.Force);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rigidbody.AddForce(Vector3.right * movePower, ForceMode.Force);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rigidbody.AddForce(Vector3.forward * movePower, ForceMode.Force);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rigidbody.AddForce(Vector3.back * movePower, ForceMode.Force);
+            }
+
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+
+        public void Test()
         {
-            rigidbody.AddForce(Vector3.forward * movePower, ForceMode.Force);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            rigidbody.AddForce(Vector3.back * movePower, ForceMode.Force);
+
         }
 
     }
-
-    public void Test()
-    {
-
-    }
-
 }
