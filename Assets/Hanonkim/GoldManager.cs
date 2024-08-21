5d1c5a2e9bd50e3548a1b7fe49a35adde9c655b2
Hanonkim <72588898+Hanonkim@users.noreply.github.com>
2024-08-21 17:16:05 +0900

    add GoldManger code
    골드 코드 추가

--- Assets/Hanonkim/GoldManager.cs
+++ Assets/Hanonkim/GoldManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class GoldManager : MonoBehaviour
 {
+    private Timer scatteringTimer = null;
+    private const int scatterInterval = 2000;
+    private int Maxgold = 0;
+    public GameObject gold;
+    GoldManager()
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ScatterGolds()
+    {
+
     }
 }
